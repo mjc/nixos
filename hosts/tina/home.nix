c48c217c87528f8689c300ba98f4f8456cf466f8
Mika Cohen <mjc@kernel.org>
2024-04-17 16:08:49 -0600

    Starship configuration

--- hosts/tina/home.nix
+++ hosts/tina/home.nix
@@ -154,6 +154,21 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
