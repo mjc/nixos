c399befe1b8c139e2877a8bc672988ed9e8cc839
Mika Cohen <mjc@kernel.org>
2024-04-17 14:41:37 -0600

    update rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,8 @@
 
 flake="${1:tina}"
 
+nix-channel --update
 # NOTE: if you are doing a major upgrade you must pass --upgrade
 sudo nixos-rebuild switch --flake "$(pwd)#$flake"
+nix-collect-garbage --delete-older-than 30d
 nix-store --optimize
