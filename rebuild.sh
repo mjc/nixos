3b29e11cbac97336cc5a8c170b13dde907cf7690
Mika Cohen <mjc@kernel.org>
2024-04-17 15:15:07 -0600

    Don't delete older generations automatically

--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,5 @@ flake="${1:tina}"
 nix-channel --update
 # NOTE: if you are doing a major upgrade you must pass --upgrade
 sudo nixos-rebuild switch --flake "$(pwd)#$flake"
-nix-collect-garbage --delete-older-than 30d
+# nix-collect-garbage --delete-older-than 30d
 nix-store --optimize
