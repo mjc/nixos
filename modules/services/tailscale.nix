e9e73b759a74b13e697fe95836f46261abde5c32
Mika Cohen <mjc@kernel.org>
2024-04-17 12:18:06 -0600

    make sure dhcp is disabled on tailscale0

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -1,6 +1,13 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "server";
   };
+
+  # https://nixos.wiki/wiki/Tailscale#Known_issues
+  networking.interfaces.tailscale0.useDHCP = lib.mkDefault false;
 }
