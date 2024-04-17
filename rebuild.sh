5a3a4666ac0175230b16459bc431153c6267f377
Mika Cohen <mjc@kernel.org>
2024-04-17 13:08:32 -0600

    Default rebuild command to my most frequently updated host

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+flake="${1:tina}"
+
 # NOTE: if you are doing a major upgrade you must pass --upgrade
-sudo nixos-rebuild switch --flake "$(pwd)#$1"
+sudo nixos-rebuild switch --flake "$(pwd)#$flake"
 nix-store --optimize
