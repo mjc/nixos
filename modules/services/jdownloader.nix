c3b118dc6f667f140813737b61f78853933e3972
Mika Cohen <mjc@kernel.org>
2024-04-17 19:04:10 -0600

    Wrong jdownloader2 path

--- modules/services/jdownloader.nix
+++ modules/services/jdownloader.nix
@@ -5,7 +5,7 @@
       jdownloader2 = {
         image = "jlesage/jdownloader-2";
         ports = ["5800:5800"];
-        volumes = ["/var/lib/jdownloader2:/config:rw" "/mnt:/downloads:rw"];
+        volumes = ["/var/lib/jdownloader2:/config:rw" "/mnt/downloads:/output:rw"];
         autoStart = true;
       };
     };
