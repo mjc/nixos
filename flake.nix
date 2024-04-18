95d991cd66281079280717a356506e0a66645f01
Mika Cohen <mjc@kernel.org>
2024-04-17 18:30:43 -0600

    Trying with docker-based jdownloader

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
         ./modules/nixos/disks.nix
 
         ./modules/services/docker.nix
+        ./modules/services/jdownloader.nix
 
         ./modules/services/tailscale.nix
         ./modules/services/avahi.nix
---
+++ modules/services/jdownloader.nix
@@ -0,0 +1,13 @@
+{...}: {
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      jdownloader2 = {
+        image = "jlesage/jdownloader-2";
+        ports = ["5800:5800"];
+        volumes = ["/var/lib/jdownloader2:/config:rw" "/mnt:/downloads:rw"];
+        autoStart = true;
+      };
+    };
+  };
+}
