f3e4642481bcdf7696b35414f205cf9c7bb7705f
Mika Cohen <mjc@kernel.org>
2024-04-19 13:45:59 -0600

    Reduce rebuild script a little

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 
-flake="${1:tina}"
-
-nix-channel --update
 # NOTE: if you are doing a major upgrade you must pass --upgrade
-sudo nixos-rebuild switch --flake "$(pwd)#$flake"
+sudo nixos-rebuild switch --refresh --flake $(pwd)
 # nix-collect-garbage --delete-older-than 30d
 nix-store --optimize
