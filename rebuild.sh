7254c742134b2788fd2b195f6437aee65b7af6bd
Mika Cohen <mjc@kernel.org>
2024-04-19 14:00:13 -0600

    make rebuild use a different command for darwin

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
 # NOTE: if you are doing a major upgrade you must pass --upgrade
-sudo nixos-rebuild switch --refresh --flake $(pwd)
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     sudo nixos-rebuild switch --refresh --flake "$(pwd)";;
+    Darwin*)    darwin-rebuild switch --flake "$(pwd)";;
+esac
 # nix-collect-garbage --delete-older-than 30d
 nix-store --optimize
