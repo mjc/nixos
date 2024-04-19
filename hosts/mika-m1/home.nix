2a1a65206874a7016c34a639c631cad4664e849d
Michael J. Cohen <mjc@kernel.org>
2024-04-19 15:28:09 -0600

    zsh config

--- hosts/mika-m1/home.nix
+++ hosts/mika-m1/home.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
@@ -18,10 +22,51 @@
     pkgs.ripgrep
     pkgs.git
 
+    pkgs.thefuck
+    pkgs.starship
+
     # stuff vscode appreciates
     pkgs.alejandra # nixos formatter
     pkgs.nil # nix language server
     pkgs.nodePackages.cspell
   ];
 
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    autocd = true; # typing /foo will do cd /foo if /foo is a directory.
+
+    enableVteIntegration = true;
+
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    history.size = 10000;
+    history.path = "${config.xdg.dataHome}/zsh/history";
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git" "thefuck" "history" "rust" "fd" "gh" "mosh" "ssh-agent" "sudo" "tmux"];
+      theme = "robbyrussell";
+      extraConfig = ''
+        PATH=$HOME/.cargo/bin:$PATH
+      '';
+    };
+  };
 }
