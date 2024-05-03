d539ee4c5c559889f94b107f87be525c673ccc1f
Michael J. Cohen <mjc@kernel.org>
2024-05-03 14:55:45 -0600

    move starship

--- hosts/mika-m1/home.nix
+++ hosts/mika-m1/home.nix
@@ -22,19 +22,4 @@
     ../../modules/home-manager/tmux.nix
     ../../modules/home-manager/zsh.nix
   ];
-
-  programs.starship = {
-    enable = true;
-    # Configuration written to ~/.config/starship.toml
-    settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
-    };
-  };
 }
--- hosts/tina/home.nix
+++ hosts/tina/home.nix
@@ -148,19 +148,4 @@
       executable = false;
     };
   };
-
-  programs.starship = {
-    enable = true;
-    # Configuration written to ~/.config/starship.toml
-    settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
-    };
-  };
 }
