1281fb6b08722914f20e9f74d20ab3fcab42b49a
Mika Cohen <mjc@kernel.org>
2024-05-09 10:21:37 -0600

    also scope x265 scripts to the correct audio channel count

--- modules/home-manager/cli-media.nix
+++ modules/home-manager/cli-media.nix
@@ -37,7 +37,7 @@
       audio="--acodec libopus --enc b:a=128k --enc ac=6"
       scale="--vfilter scale=1920:-2"
       av1cmd="ab-av1 auto-encode ''${video} ''${scale}  ''${audio} --scd true --cache true --keyint 30s -i"
-      fd . -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd 5\.1 -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "x265-8ch" ''
       x265="-e libx265 --preset medium"
@@ -45,7 +45,7 @@
       audio="--acodec libopus --enc b:a=256k --enc ac=8"
       scale="--vfilter scale=1920:-2"
       av1cmd="ab-av1 auto-encode $\{video} $\{scale} ''${audio} --scd true --cache true --keyint 30s -i"
-      fd . -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd 7\.1 -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "x265-stereo" ''
       x265="-e libx265 --preset medium"
@@ -53,7 +53,7 @@
       audio="--acodec libopus --enc b:a=48k --enc ac=2"
       scale="--vfilter scale=1920:-2"
       av1cmd="ab-av1 auto-encode ''${video} ''${scale}  ''${audio} --scd true --cache true --keyint 30s -i"
-      fd . -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd 2\.0 -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
     '')
   ];
 }
