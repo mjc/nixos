5458a0c5a42e2d81c2477d1bb06efb86bf636027
Michael J. Cohen <mjc@kernel.org>
2024-05-13 14:11:34 -0600

    add channel update to rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -3,8 +3,12 @@
 # NOTE: if you are doing a major upgrade you must pass --upgrade
 operating_system="$(uname -s)"
 case "${operating_system}" in
-    Linux*)     doas nixos-rebuild switch --refresh --flake "$(pwd)";;
-    Darwin*)    darwin-rebuild switch --refresh --flake "$(pwd)";;
+    Linux*)
+        doas nix-channel --update
+        doas nixos-rebuild switch --refresh --flake "$(pwd)";;
+    Darwin*)
+        nix-channel --update
+        darwin-rebuild switch --refresh --flake "$(pwd)";;
 esac
 # nix-collect-garbage --delete-older-than 30d
 nix-store --optimize
