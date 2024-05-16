86072cf6689ee27db0a69e30514538a270fc9070
Mika Cohen <mjc@kernel.org>
2024-05-16 09:42:36 -0600

    still trying to figure out systemd services

--- modules/services/glances.nix
+++ modules/services/glances.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   # TODO: figure out why services.glances.enable = true; does not work
   systemd.services.glances = {
+    enable = true;
     wantedBy = ["multiuser.target"];
     after = ["network.target"];
     description = "Start the glances web ui";
