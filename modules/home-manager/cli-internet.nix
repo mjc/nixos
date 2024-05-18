8401256971c16d9bd12f6cbfa7491d678737c82a
Michael J. Cohen <mjc@kernel.org>
2024-05-18 10:14:36 -0600

    Try adding known hosts

--- modules/home-manager/cli-internet.nix
+++ modules/home-manager/cli-internet.nix
@@ -6,4 +6,16 @@
 
     mosh
   ];
+
+  security.pam.sshAgentAuth.enable = true;
+  programs.ssh.knownHosts = {
+    "tina" = {
+      publicKeyFile = "ssh-hosts/tina";
+      hostNames = [
+        "tina"
+        "tina.local"
+        "tina.lan.325i.org"
+      ];
+    };
+  };
 }
