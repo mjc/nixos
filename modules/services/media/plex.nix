7f66edcb5a3f5e12a49caa10182f0855f190d328
Mika Cohen <mjc@kernel.org>
2024-06-03 11:56:06 -0600

    Update plex

--- modules/services/media/plex.nix
+++ modules/services/media/plex.nix
@@ -2,10 +2,10 @@
   services.plex = let
     plexpass = pkgs.plex.override {
       plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-        version = "1.40.2.8395-c67dce28e";
+        version = "1.40.3.8530-ac2fe9b3c";
         src = pkgs.fetchurl {
           url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-          sha256 = "sha256-gYRhQIf6RaXgFTaigFW1yJ7ndxRmOP6oJSNnr8o0EBM=";
+          sha256 = "sha256-A/2nv7kZnolC+i+k54N5WZ5U8WaAy7g7UyeCAxwTQps=";
         };
       });
     };
