b13717d47e83d0b4fa0a7dc76ea3e3e22a9345f5
Mika Cohen <mjc@kernel.org>
2024-06-04 16:10:20 -0600

    set group id for jdownloader2

--- modules/services/jdownloader2.nix
+++ modules/services/jdownloader2.nix
@@ -2,6 +2,11 @@
   virtualisation.oci-containers.containers.jdownloader2 = {
     image = "jlesage/jdownloader-2";
     ports = ["5800:5800"];
+    environment = {
+      USER_ID = "1000";
+      GROUP_ID = "994";
+      UMASK = "017";
+    };
     volumes = ["/var/lib/jdownloader2:/config:rw" "/mnt/downloads:/output:rw"];
     autoStart = true;
   };
