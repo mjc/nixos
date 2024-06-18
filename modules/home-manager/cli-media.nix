5b3438147f3f11bc9a32f7cda71e5842038acbfc
Mika Cohen <mjc@kernel.org>
2024-06-18 17:33:02 -0600

    Skip atmos so I don't accidentally fuck that audio up

--- modules/home-manager/cli-media.nix
+++ modules/home-manager/cli-media.nix
@@ -13,7 +13,7 @@
       # grain="--svt film-grain=8 --svt tune=0 --svt film-grain-denoise=0"
       filter="--vfilter scale=1920:-2 ''${grain}"
       av1cmd="ab-av1 auto-encode ''${video} ''${filter}  ''${audio} --scd true --cache true --keyint 30s -i"
-      fd '5\.1' -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd '5\.1' -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "av1-8ch" ''
       video="--pix-format yuv420p10le"
@@ -21,7 +21,7 @@
       # grain="--svt film-grain=8 --svt tune=0 --svt film-grain-denoise=0"
       filter="--vfilter scale=1920:-2 ''${grain}"
       av1cmd="ab-av1 auto-encode ''${video}  ''${filter}  ''${audio} --scd true --cache true --keyint 30s -i"
-      fd '7\.1' -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd '7\.1' -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "av1-stereo" ''
       video="--pix-format yuv420p10le"
@@ -29,7 +29,7 @@
       # grain="--svt film-grain=8 --svt tune=0 --svt film-grain-denoise=0"
       filter="--vfilter scale=1920:-2 ''${grain}"
       av1cmd="ab-av1 auto-encode ''${video} ''${filter}  ''${audio} --scd true --cache true --keyint 30s -i"
-      fd '2\.0' -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd '2\.0' -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "av1-stereo-film-grain" ''
       video="--pix-format yuv420p10le"
@@ -37,7 +37,7 @@
       grain="--svt film-grain=8 --svt tune=0 --svt film-grain-denoise=0"
       filter="--vfilter scale=1920:-2 ''${grain}"
       av1cmd="ab-av1 auto-encode ''${video} ''${filter} ''${grain} ''${audio} --scd true --cache true --keyint 30s -i"
-      fd '2\.0' -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd '2\.0' -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "x265-6ch" ''
       x265="-e libx265 --preset medium"
@@ -45,7 +45,7 @@
       audio="--acodec libopus --enc b:a=128k --enc ac=6"
       scale="--vfilter scale=1920:-2"
       av1cmd="ab-av1 auto-encode ''${video} ''${scale}  ''${audio} --scd true --cache true --keyint 30s -i"
-      fd 5\.1 -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd 5\.1 -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "x265-8ch" ''
       x265="-e libx265 --preset medium"
@@ -53,7 +53,7 @@
       audio="--acodec libopus --enc b:a=256k --enc ac=8"
       scale="--vfilter scale=1920:-2"
       av1cmd="ab-av1 auto-encode ''${video} ''${scale} ''${audio} --scd true --cache true --keyint 30s -i"
-      fd 7\.1 -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd 7\.1 -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
     (writeShellScriptBin "x265-stereo" ''
       x265="-e libx265 --preset medium"
@@ -61,7 +61,7 @@
       audio="--acodec libopus --enc b:a=48k --enc ac=2"
       scale="--vfilter scale=1920:-2"
       av1cmd="ab-av1 auto-encode ''${video} ''${scale}  ''${audio} --scd true --cache true --keyint 30s -i"
-      fd 2\.0 -E '*720*' -E '*Opus*' -e mkv -j1 -x ''${av1cmd}
+      fd 2\.0 -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
   ];
 }
