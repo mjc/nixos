92c44edac553f9ffedcba0a9cec9893db6dac84f
Mika Cohen <mjc@kernel.org>
2024-06-24 11:52:33 -0600

    remove nightly-only rust flag

--- modules/home-manager/dev-tools.nix
+++ modules/home-manager/dev-tools.nix
@@ -26,11 +26,11 @@
       text = ''
         [target.x86_64-apple-darwin]
         # pkgs.zld when I conditionalize it
-        rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold", "-C", "target-cpu=native", "-Z", "threads=8"]
+        rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold", "-C", "target-cpu=native"]
 
         [target.x86_64-unknown-linux-gnu]
         linker = "${pkgs.clang}/bin/clang"
-        rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold", "-C", "target-cpu=native", "-Z", "threads=8"]
+        rustflags = ["-C", "link-arg=--ld-path=${pkgs.mold}/bin/mold", "-C", "target-cpu=native"]
       '';
       executable = false;
     };
