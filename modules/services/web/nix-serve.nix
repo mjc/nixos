7eaf942d8131c10e4db21f906f1bb0841db40b9f
Mika Cohen <mjc@kernel.org>
2024-06-25 11:03:41 -0600

    Fix nix-serve

--- modules/services/web/nix-serve.nix
+++ modules/services/web/nix-serve.nix
@@ -2,6 +2,8 @@
   services.nix-serve = {
     enable = true;
     secretKeyFile = "/var/lib/nix-cache/cache-priv-key.pem";
+    port = 5101;
+    bindAddress = "127.0.0.1";
   };
 
   services.nginx.virtualHosts."nixcache.325i.org" = {
@@ -10,7 +12,6 @@
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
-      proxyWebsockets = true; # needed if you need to use WebSocket
     };
   };
 }
