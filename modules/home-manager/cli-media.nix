54422a1d5039a6f77dd009177ebc4f9cdd185b38
Mika Cohen <mjc@kernel.org>
2024-06-28 23:14:52 -0600

    Add 6ch film grain for av1

--- modules/home-manager/cli-media.nix
+++ modules/home-manager/cli-media.nix
@@ -30,6 +30,14 @@
       av1cmd="ab-av1 auto-encode --temp-dir $HOME/.ab-av1 ''${video} ''${filter}  ''${audio} --scd true --cache true --keyint 30s -i"
       fd '5\.1' -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
     '')
+    (writeShellScriptBin "av1-6ch-film-grain" ''
+      video="--pix-format yuv420p10le"
+      audio="--acodec libopus --enc b:a=128k --enc ac=6"
+      grain="--svt film-grain=8 --svt tune=0 --svt film-grain-denoise=0"
+      filter="--vfilter scale=1920:-2 ''${grain}"
+      av1cmd="ab-av1 auto-encode --temp-dir $HOME/.ab-av1 ''${video} ''${filter} ''${grain} ''${audio} --scd true --cache true --keyint 30s -i"
+      fd '5\.1' -E '*720*' -E '*Opus*' -E '*Atmos*' -e mkv -j1 -x ''${av1cmd}
+    '')
     (writeShellScriptBin "av1-stereo" ''
       video="--pix-format yuv420p10le"
       audio="--acodec libopus --enc b:a=48k --enc ac=2"
