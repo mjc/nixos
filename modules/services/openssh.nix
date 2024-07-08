9ea7fab6d782c980b859f35841a59120c830e7e8
Mika Cohen <mjc@kernel.org>
2024-07-08 14:58:41 -0600

    switch to endlessh-go because I might eventually want a dashboard

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -1,5 +1,11 @@
 {...}: {
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.endlessh.enable = true;
+  services.endlessh-go = {
+    enable = true;
+    extraOptions = [
+      "--logtostderr"
+      "-v=1"
+    ];
+  };
 }
